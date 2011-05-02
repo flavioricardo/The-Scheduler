fbc24a7add3e6a6c54bdfc66b6109ab4310031bf
Flávio Ricardo <flavioricardo@live.com>
2011-05-02 18:55:09 -0400

    First commit

---
+++ src/EscalonamentoProcessos/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EscalonamentoProcessos;
+
+/**
+ *
+ * @author flavio
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Home().setVisible(true);
+    }
+}
